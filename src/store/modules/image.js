703f5743c681a5c899611ce3e772d5a442ecf0f1
Mavluts <mavluts@MacBook-Pro-Mavluts.local>
2020-11-20 20:46:01 +0300

    Добавил загрузку 15 изображений при первичном открытии страницы История, добавил индикацию загрузки - компонент Loader

--- src/store/modules/image.js
+++ src/store/modules/image.js
@@ -1,37 +1,51 @@
 export default {
   actions: {
-    async fetchImages(ctx) {
-      const IMAGES_COUNT = 15;
-      const images = new Array(IMAGES_COUNT);
-
-      
-      for(let i = 0; i < images.length; i++) {
-        const response = await fetch(
-          'https://api.giphy.com/v1/gifs/random?api_key=xgcnvYuqk4vP1WQQtWPz6F1A0B4WHHdA'
-        );
-        const responseData = await response.json();
-        const image = {};
-        image.id = responseData.data.id;
-        image.src = responseData.data.images.original.url;
-        image.alt = responseData.data.title.trim() ? responseData.data.title.trim() : 'random gif';
-        images[i] = image;
+    async fetchImages({commit, state}) {
+      if(state.images.length === 0) {
+        const IMAGES_COUNT = 15;
+        const queries = new Array(IMAGES_COUNT);
+        const images = [];
+        for(let i = 0; i < queries.length; i++) {
+          queries[i] = fetch(
+            'https://api.giphy.com/v1/gifs/random?api_key=xgcnvYuqk4vP1WQQtWPz6F1A0B4WHHdA'
+          )
+        }
+        Promise.all(queries)
+          .then((responses) => {
+            responses.forEach(async (response) => {  
+              const responseData = await response.json();
+              const image = {};
+              image.id = responseData.data.id;
+              image.src = responseData.data.images.original.url;
+              image.title = responseData.data.title.trim() ? responseData.data.title.trim() : 'random gif';
+              images.push(image);
+            })
+          })
+          .then(() => {
+            commit('updateImages', images);
+            commit('setLoading', false);
+          })
       }
-
-
-      ctx.commit('updateImages', images);
     }
   },
   mutations: {
     updateImages(state, images) {
       state.images = images;
+    },
+    setLoading(state, loading) {
+      state.loading = loading;
     }
   },
   state: {
-    images: []
+    images: [],
+    loading: true
   },
   getters: {
-    images(state) {
+    images(state){
       return state.images;
+    },
+    loading(state) {
+      return state.loading;
     }
   }
 }
--- src/views/History.vue
+++ src/views/History.vue
@@ -1,27 +1,35 @@
 <template>
   <section>
     <h1>История</h1>
-    <div class="pictures-history__container">
+    <Loader v-if="loading"/>
+    <div 
+      v-else
+      class="pictures-history__container"
+    >
       <img 
-        v-for="image in images" :key="image.id"
+        v-for="image in images" :key="image.id + Date.now()"
         v-bind:image="image"
         :src="image.src" 
-        :alt="image.alt"
+        :alt="image.title"
       >
     </div>
+    
   </section>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import Loader from '@/components/Loader';
 
 export default {
   name: 'History',
-  computed: mapGetters(['images']),
+  components: {
+    Loader
+  },
+  computed: mapGetters(['images', 'loading']),
   methods: mapActions(['fetchImages']),
   async mounted() {
     this.fetchImages();
   }
 }
-
 </script>
